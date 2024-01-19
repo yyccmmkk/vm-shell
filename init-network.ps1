c787545d00553479c4f92d9978e57e1a37fec93d
zhoulongfei <36995800@163.com>
2024-01-19 17:51:25 +0800

    网络开荒设置

---
+++ init-network.ps1
@@ -0,0 +1,5 @@
+# NAT模式 网络开荒配置
+scp ./sh/network.sh root@127.0.0.2:/root/
+#后续执行初始化脚本
+chmod +x ./network.sh
+sh ./network.sh
--- sh/network.sh
+++ sh/network.sh
@@ -1,8 +1,9 @@
 # NAT 网卡模式联网设置
 
+fileName= `find /etc/sysconfig/network-scripts/ -name "ifcfg-enp0s*"`
 #上网设置
-sed -i -r 's/^(ONBOOT=)(yes|no)/\1yes/g' /etc/sysconfig/network-scripts/ifcfg-enp0s3
-sed -i -r 's/^(BOOTPROTO=)[a-z]+/\1dhcp/g' /etc/sysconfig/network-scripts/ifcfg-enp0s3
+sed -i -r 's/^(ONBOOT=)(yes|no)/\1yes/g' /etc/sysconfig/network-scripts/$fileName
+sed -i -r 's/^(BOOTPROTO=)[a-z]+/\1dhcp/g' /etc/sysconfig/network-scripts/$fileName
 systemctl restart network
 # 本地开发端口开放设置
 firewall-cmd --add-port=22/tcp --permanent
@@ -10,3 +11,5 @@ firewall-cmd --add-port=80/tcp --permanent
 firewall-cmd --add-port=4200/tcp --permanent
 firewall-cmd --add-port=443/tcp --permanent
 firewall-cmd --reload
+#清理文件
+rm -f ./network.sh

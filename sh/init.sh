8e90d52578c5c1dfda25dbee942a901c5318e665
zhoulongfei <36995800@163.com>
2024-01-22 10:56:38 +0800

    feat:kms服务安装脚本

---
+++ sh/init.sh
@@ -0,0 +1,4 @@
+yum update
+sudo yum groupinstall "Development Tools"  
+sudo yum install zlib-devel bzip2-devel openssl-devel ncurses-devel
+yum install -y git 
---
+++ sh/kms.sh
@@ -0,0 +1,18 @@
+#仓库不可用直接复制
+#git clone git@github.com:Wind4/vlmcsd.git
+tar -xzvf vlmcsd.tar.gz 
+cd vlmcsd
+make
+
+cp ./bin/vlmcsd  /usr/bin/
+chmod 755 /usr/bin/vlmcsd
+
+touch /var/run/vlmcsd.pid
+chmod 755 /var/run/vlmcsd.pid
+echo -e "[Unit]\nDescription=KMS Emulator\nAfter=network.target\n\n[Service]\nType=forking\nPIDFile=/var/run/vlmcsd.pid\nExecStart=/usr/bin/vlmcsd -p /var/run/vlmcsd.pid\nExecStop=/bin/kill -HUP $MAINPID\nPrivateTmp=true\n\n[Install]\nWantedBy=multi-user.target" > /lib/systemd/system/vlmcsd.service
+systemctl daemon-reload
+systemctl enable vlmcsd.service
+systemctl start  vlmcsd.service
+firewall-cmd --zone=public --add-port=1688/tcp --permanent
+firewall-cmd --reload
+rm kms.sh
